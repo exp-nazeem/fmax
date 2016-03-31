2d2eada6a23bb92c87cf61dda3207d3c77a96f1e
exp-nazeem <muhammed.nazeem@experionglobal.com>
2016-03-31 15:59:06 +0530

    Content Changes

--- local/app/views/faq.blade.php
+++ local/app/views/faq.blade.php
@@ -15,44 +15,44 @@
                         <div class="container" >
         <div class="settings-content-block">           
             <h2>1.  What is FieldMax®?</h2>
-                <p>FieldMax® is a suite of mobile and web based applications used for field sales automation. It has     two components - FieldMax® Mobile and FieldMax® Office.</h3>
+                <p>FieldMax® is a suite of mobile and web-based applications used for field sales automation. It has    two components - FieldMax® Mobile and FieldMax® Office.</h3>
                 <ul class="content-sub-listing">
-                    <li><span>a)    FieldMax® Mobile application is used by the field staff to enter field sales information on          a simple Java/Android enabled mobile handset. </span></li>
-                    <li><span>b)    FieldMax® Office is a web based application used by the operations team to track and     process the field sales data.</span></li>
+                    <li><span> FieldMax® Mobile application is used by the field staff to enter field sales information on a simple Java/Android enabled mobile handset.</span></li>
+                    <li><span> FieldMax® Office is a web-based application used by the operations team to track and     process the field sales data.</span></li>
                 </ul>
 
                 <h2>2.  What kind of business can get benefited out of FieldMax®?</h2>
-                <p>FieldMax® would be definite fit in organisations who has field sales team working in the market,  meeting more than 15 customer per day, entering sales data in Paper and DSR sheets, executing   primary/secondary sales, meeting distributors/C&F agent reporting end of the day sales data to  reporting managers over calls/SMS/emails etc</p>
+                <p>FieldMax® would be definite fit in organisations who has field sales team working in the market, meeting more than 15 customer per day, entering sales data in Paper and DSR sheets, executing primary/secondary sales, meeting distributors/C&F agent reporting end of the day sales data to reporting managers over calls/SMS/emails etc</p>
 
-                <h2>3.  Do I need to have a smart phone to run FieldMax® application.</h2>
-                <p>No. It is not mandatory that you must have a smart phone to use FieldMax® application. The only   requirement is the device should be Java/Android/Windows supportive with internet   connectivity. Our team would advise you the best device as per your business requirement.</p>
+                <h2>3. Do I need to have a smartphone to run FieldMax® application?</h2>
+                <p>No. It is not mandatory that you must have a smartphone to use FieldMax® application. The only   requirement is the device should be Java/Android/Windows supportive with internet connectivity. Our team would advise you the best device as per your business requirement.</p>
 
                 <h2>4.  What is the time required for implementing FieldMax® in my organisation?</h2>
-                <p>Implementation time is depended on many factors like master data availability, teams readiness   etc. For a normal scenario it would take 15 - 30 days for a full roll out comprising of 100 member sales team.</p>
+                <p>Implementation time depends on many factors like master data availability, teams readiness etc. For a normal scenario it would take 15 - 30 days for a full roll out comprising of 100 member sales team.</p>
 
-                <h2>5.  Can I see the real time data of my field sales team. How does the data transfer happens.</h2>
-                <p>Yes. All the data transaction through FieldMax® is visible to authorised users in real time. The data is shown to the user based on his privileges.<br><br> The data transfer between mobile application and web application is through GPRS.</p>
+                <h2>5. Can I see the real-time  data of my field sales team? How does the data transfer happen?</h2>
+                <p>Yes. All the data transaction through FieldMax® is visible to authorised users in real-time . The data is shown to the user based on his privileges. The data transfer between the mobile application and web application is through GPRS.</p>
 
-                <h2>6.  Can I see the location of my sales person in the field using GPS </h2>
+                <h2>6. Can I see the location of my sales person in the field using GPS? </h2>
                 <p>Yes. All transaction executed through mobile application also captures the GPS coordinates.</p>
 
-                <h2>7.  Is there any limit on the number of users that can be registered in FieldMax® application</h2>
+                <h2>7. Is there any limit on the number of users that can be registered in FieldMax® application ?</h2>
                 <p>No. There is no limit on the number of users that can be registered through FieldMax® mobile application.</p>
 
-                <h2>8.  After the data is captured through mobile application, where is the data stored?</h2>
-                <p>FieldMax® is a cloud hosted application. FieldMax® uses Microsoft cloud platform for server and    data storage. Data security and privacy is also ensured at its best.<br><br>As an added advantage, if required FieldMax® can also be installed at your in-house facility.</p>
+                <h2>8. After the data is captured through a mobile application, where is the data stored?</h2>
+                <p>FieldMax® is a cloud hosted application. FieldMax® uses Microsoft cloud platform for server and  data storage. Data security and privacy is also ensured at its best. As an added advantage, if required FieldMax® can also be installed at your in-house facility.</p>
 
-                <h2>9.  Can I interface FieldMax® application with my ERP applications in house?</h2>
+                <h2>9.  Can I interface FieldMax® application with my ERP applications in-house?</h2>
                 <p>Yes. FieldMax® can be interfaced with any 3rd party ERP applications like SAP, Tally etc.</p>
 
                 <h2>10. How can I subscribe to FieldMax®</h2>
                 <p>FieldMax® is available in both SAAS ( Software as a service) and License mode. You can reach to one of the contact number mentioned to get more details about the pricing.</p>
 
                 <h2>11. What is the difference between SAAS and License mode</h2>
-                <p>Under SAAS mode you can subscribe FieldMax® on a monthly per user rental mode. You need to pay only for the number of active user you have in your team. This would be in a monthly recurring mode as long as you would like to use the application in your organisation Under License purchase you can purchase the number of license as a one time-life time fee. No recurring or renewal fee. </p>
+                <p>Under SAAS mode, you can subscribe FieldMax® on a monthly per user rental mode. You need to pay only for the number of active users you have on your team. This would be in monthly recurring model as long as you would like to use the application in your organisation. Under License purchase, you can purchase the number of license as a one time fee. No recurring or renewal fee. </p>
 
                 <h2>12. How would you support me after the implementation is completed?</h2>
-                <p>FieldMax® is available both in SAAS and License mode. Under SAAS mode the support is part of the monthly SAAS fee. Under License mode, you can subscribe for the AMC contract after expiry of the warranty period.</p><br><br>
+                <p>FieldMax® is available both in SAAS and License mode. Under SAAS mode, the support is part of the monthly SAAS fee. Under License mode, you can subscribe for the AMC contract after expiry of the warranty period.</p><br><br>
 
 
 
@@ -63,6 +63,6 @@
                     </div>
                 </div>
                 
-            </div>
+            </div> <br><br>
 
 @endsection

eb6cea8212a06e9717ee181797d0d38dc9404201
exp-nazeem <muhammed.nazeem@experionglobal.com>
2016-06-13 12:00:00 +0530

    Latest news: changes done by Sajith

--- local/app/views/latestNews.blade.php
+++ local/app/views/latestNews.blade.php
@@ -3,18 +3,25 @@
 
 <br><br>
 
-     <div class="container text-center">
+    <div class="container text-center">
         <div class="section-title center">
             <br>
             <br>
-            <h2>FieldMax® update - version 2.5.3</h2>
+            <h2>Product update</h2>
             <div class="line">
                 <hr>
             </div>
             <div class="clearfix"></div>
             <div style="font-size: larger;color: #6F6F6F;">
                 <div class="text-justify">
-                    <p>FieldMax® product suite is now upgraded with new version release. With this version upgrade to 2.5.3, FieldMax® offers optional features for the existing users to migrate to new and advance functional offering. FieldMax® is one of the best and feature rich sales force automation application currently available in the market. FieldMax® application is superior to any other similar solutions in the market through its continuous upgrades and functional offerings. The new version upgrade provides the users with unique features like Asset auditing, performance enhancement of both mobile and backend application and specific customer requirements. FieldMax® is currently subscribed by more than ten thousand plus users serving the requirements of both small and medium scale companies to organisations with more than 1000 plus sales team. FieldMax® is available for subscription on per month user fee model and one time license fee as per the commercial fitment to the customer</p>
+                    <h2 style="font-size: 30px;">FieldMax® version 2.5.3</h2>
+                    <br>
+                    <p>FieldMax® sales force automation solution is currently deployed in more than 200 organisations across India, Africa and Middle east region. FieldMax® has been serving the requirement of the organisations that include small and medium scale companies with average field team size of 25 sales team to large and well known global organisations with more than 1000 plus direct and indirect sales team members.
+                        <br>
+                        <br>The latest version update of FieldMax® (version 2.5.3) adds new functional features like Merchandising, Asset Audit etc. It also enhances the order taking function through the incorporation of facility to handle Must Sell & Focus/Golden Products.
+                        <br>
+                        <br>
+                    </p>
                 </div>
             </div>
         </div>
@@ -23,21 +30,24 @@
         <div class="container text-center">
             <div class="section-title center">
                 <br>
-                <h2>Merchandising and Asset Audit</h2>
-                <div class="line">
-                    <hr>
-                </div>
                 <div class="clearfix"></div>
                 <div style="font-size: larger;color: #6F6F6F;">
                     <div class="row">
+                        <div class="col-lg-12 text-left">
+                            <h2 style="font-size: 30px;">Merchandising and Asset Audit</h2>
+                            <br>
+                            <br>
+                        </div>
                         <div class="col-xs-6">
                             <img src="assets/images/female.jpg" class="img-responsive" alt="Image">
                         </div>
                         <div class="col-xs-6">
                             <div class="text-justify">
-                                <p>Companies spend lot of effort and resources for in-store promotions. The objective of these promotions are to generate more demand for their products, especially at a time when competition is at its heights and the line of differentiation is marginal. Keeping track of the spend and ensuring that the promotional campaigns are executed properly is a challenge for everyday. While it is possible to get the data from few organised stores and retail chains, the cost of labour involved in this data collection makes things much more challenging. On top comes the challenge in getting the field data in time without which the exercise would be futile.
+                                <p>Companies often invest heavily in providing hardware assets like refrigerators to the retail stores as part of their retail execution plan. This is in addition to other investments made in the retail shelf space.
                                     <br>
-                                    <br>FieldMax® mobile applications is equipped with merchandising and asset audit tools that delivers critical insight into the merchandising and promotional activities and enable back office team to manage them better. The application enables the field representative to take live data including digital images in real time without limiting the number of transaction execution. FieldMax® office backend application assist the management team to share digital data to field team everyday which assists better transactional accuracy.
+                                    <br> Executing audits to track the effective execution of merchandising plans involves large investment in merchandiser manpower. Tracking the utilisation and productivity of the hardware assets as part of the merchandising audit is an additional challenge. Merchandiser and Asset Auditors often struggle in getting or sharing the audit data owing to the manual flow of information through documents and e-mails.
+                                    <br>
+                                    <br> FieldMax® mobile applications is equipped with merchandising and asset audit tools that delivers critical insight into the merchandising and promotional activities and enable back office team to manage them better. The application enables the field representative capture live data including digital images in real time without limiting the number of transaction execution. FieldMax® office backend application assists the management team to share digital data to field team everyday while facilitating better transactional accuracy.
                                 </p>
                             </div>
                         </div>
@@ -47,14 +57,14 @@
         </div>
         <div class="container text-left">
             <div class="section-title">
-                <h2>Business Benefit:</h2>
+                <h2 style="font-size: 30px;">Business Benefit:</h2>
                 <div class="line">
                     <hr>
                 </div>
                 <div class="clearfix"></div>
                 <div style="font-size: larger;">
                     <div class="text-justify">
-                        <ul class="about-list">
+                        <ul class="about-list" style="color: rgb(109, 109, 109);">
                             <li><span class="fa fa-dot-circle-o"></span> Real time inputs to gauge effectiveness of merchandising, marketing, and promotional strategies</li>
                             <li><span class="fa fa-dot-circle-o"></span> Reduce out-of-stock situations and maintain shelf availability (fill rates) on promotional and fast moving items cost-effectively</li>
                             <li><span class="fa fa-dot-circle-o"></span> Provides inputs to design store planogram based on critical in store inputs like shopper paths, concentration, and category-affinity in the retail store</li>
@@ -69,16 +79,17 @@
     <div class="container text-center">
         <div class="section-title center">
             <br>
-            <h2>"Focus products" and "MUST SELL" alerts</h2>
-            <div class="line">
-                <hr>
-            </div>
             <div class="clearfix"></div>
             <div style="font-size: larger;color: #6F6F6F;">
                 <div class="row">
+                    <div class="col-lg-12 text-left">
+                        <h2 style="font-size: 30px;">"Focus products" and "Must sell" alerts</h2>
+                        <br>
+                        <br>
+                    </div>
                     <div class="col-xs-6">
                         <div class="text-justify">
-                            <p>FieldMax® office and mobile application is now enhanced to provide alerts to the transaction executioner about ‘MUST SELL’ and "FOCUS PRODUCT". The backend office team can select the focus item from the list of product/SKU with much ease through FieldMax® office application. During sales execution the field team get alerts in the mobile application about the MUST SELL’ and high value products based on the nature and category of the customer he deals with. The focus products can be classified on a regional basis based on the market condition and field demands.
+                            <p>FieldMax® office and mobile application is now enhanced to provide alerts to the transaction executioner about ‘Must sell’ and "Focus products". The backend office team can select the focus item from the list of product/SKU with much ease through FieldMax® office application. During sales execution the field team get alerts in the mobile application about the ‘Must sell’ and high priority products based on the nature and category of the customer he deals with. The focus products can be classified on a regional basis based on the market condition and field demands.
                                 <br>
                                 <br> The display of these alerts can be in the form of different colour codes or highlight indicators.
                             </p>
@@ -93,14 +104,14 @@
     </div>
     <div class="container text-left">
         <div class="section-title">
-            <h2>Business Benefit:</h2>
+            <h2 style="font-size: 30px;">Business Benefit:</h2>
             <div class="line">
                 <hr>
             </div>
             <div class="clearfix"></div>
             <div style="font-size: larger;">
                 <div class="text-justify">
-                    <ul class="about-list">
+                    <ul class="about-list" style="color: rgb(109, 109, 109);">
                         <li><span class="fa fa-dot-circle-o"></span> Proactive alerts to field personal for effective sales call</li>
                         <li><span class="fa fa-dot-circle-o"></span> Real time data exchange to meet and exceed sales goals</li>
                         <li><span class="fa fa-dot-circle-o"></span> Better clarity to sell leading brands to assigned retail accounts in chosen territory</li>
@@ -111,13 +122,10 @@
         </div>
     </div>
     <div class="background-007">
-        <div class="container text-center">
+        <div class="container text-left">
             <div class="section-title center">
                 <br>
-                <h2>Order cancellation</h2>
-                <div class="line">
-                    <hr>
-                </div>
+                <h2 style="font-size: 30px;">Optimised for speed and performance<br><br></h2>
                 <div class="clearfix"></div>
                 <div style="font-size: larger;color: #6F6F6F;">
                     <div class="row">
@@ -126,7 +134,9 @@
                         </div>
                         <div class="col-xs-8">
                             <div class="text-justify">
-                                <p>Often it becomes a challenge for sales organisations to manage sales order cancellation. The most common scenario is, after the sales person visits the retail counter and captures the sales order, the retail counter could either modify the sales order already registered or cancel the sales order in full. To cope with this scenario FieldMax® mobile users can now cancel the sales orders registered by them during the day through the mobile application. This is possible provided the backend team hasn`t processed the sales order through the FieldMax® office backend application. In such case the field user can retrieve the sales data in a separate field listed in the mobile application and make necessary modification and alterations.
+                                <p>FieldMax® mobile application is now further optimised for speed and performance. This enhancement improves the performance of the mobile application reducing the time delay during the operational execution of transactions from the field. Through this upgrade, FieldMax® mobile application can refresh the device memory allocation automatically for optimal performance. This means lesser application error and more sales time for your field team. The enhancement strengthens the user interface of FieldMax® mobile application making it more responsive and can now handle more than 3500 line items including digital images of the product/SKU.
+                                    <br>
+                                    <br> This enhancement will enable the field user to execute the transaction faster and easier making each of their sales call more productive. This enhancement will also help organisations with larger SKU to list the products in multiple chronological order based on the business and field requirement. Inventory and audit department shall also benefit helping them to easily handle the field requirement of their team.
                                 </p>
                             </div>
                         </div>
@@ -134,7 +144,8 @@
                 </div>
             </div>
         </div>
+        <br>
+        <br>
     </div>
- <br>
 
 @endsection
